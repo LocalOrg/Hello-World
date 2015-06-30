2f51253e05586ac7333a5d66d2def905aeecc30d
kliferd <kliferd@users.noreply.github.com>
2015-06-30 16:26:09 +0530

    Update Program.cs

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,7 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World !!");
-            Console.WriteLine("Hello Clinton !!");
+            Console.WriteLine("Hello Clinton How are you !!");
         }
     }
 }
